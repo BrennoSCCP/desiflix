c684cc40c9ed05758e741ee4a50bc50561ee61f5
BrennoSCCP <brennoohara@gmail.com>
2026-03-06 09:43:32 -0400

    arrumei a pagina de acesso dos filmes, com as informações do filme e ajustei tbm o error

--- src/pages/Filmes/index.js
+++ src/pages/Filmes/index.js
@@ -1,9 +1,55 @@
+import {useState, useEffect} from 'react';
+import { useParams } from "react-router-dom";
+import api from '../../services/api';
 
 function Filme(){
+    const { id } = useParams();
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        async function loadFilme(){
+            const response = await api.get(`/movie/${id}`, {
+                params:{
+                    api_key: "cfb58711e7afb393a4b54db263d762fa",
+                    language: "pt-BR",
+                }
+            })
+            .then((response)=>{
+                setFilme(response.data);
+                setLoading(false);
+            })
+            .catch(()=>{
+                console.log("Filme não encontrado!");
+            });
+        }
+
+        loadFilme();
+
+        return() => {
+            console.log("Componente desmontado");
+        }
+    }, []);
+
+    if(loading){
+        return(
+            <div className="filme-info">
+                <h1>Carregando detalhes...</h1>
+            </div>
+        )
+    }
+
     return(
-        <div className="favoritos">
-            <h1>Bem vindo a página detalhes do Filme</h1>
-        </div>
+        <div className="filme-info">
+            <h1>{filme.title}</h1>
+            <img src = {`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title}/>
+
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+
+            <strong>Avaliação: {filme.vote_average} / 10</strong>
+            
+        </div>      
     )
 }
 
